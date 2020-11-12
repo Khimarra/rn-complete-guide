fd998438dc2494c7a43b092bf1411c6ead457454
khimarra <tsolbrig87@gmail.com>
2020-11-12 18:15:58 -0500

    add textinput and button with some styles

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // inner pair of curly braces in style property is just a JS object
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput
+          // TextInput doesn't have its own structure, had to add padding to the whole view to give it space on the page
+          placeholder="Course Goal"
+          // note that styles are camelCase here. it's JS, not CSS
+          style={{ borderBottomColor: "black", borderBottomWidth: 2, width: '80%' }}
+        />
+        <Button title="ADD" />
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({})
